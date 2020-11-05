b77291cdb1bf3bd16e5527a679410aa74726eb01
Kim Young Ae <kya754@gmail.com>
2020-11-05 18:29:18 +0900

    메인 작업중

--- common/js/layout.js
+++ common/js/layout.js
@@ -1,24 +1,40 @@
-// //header 가로스크롤
 
-function bodyWidthSet(){
-    $('body').css({
-        'width' : window.innerWidth
+$(document).ready(function(){
+    // //header 가로스크롤
+
+    // main.html 에서만 적용
+    function bodyWidthSet(){
+        $('body.main').css({
+            'width' : window.innerWidth
+        });
+
+        if($('body.main').width() < 1550) {
+            $('body.main').css({
+                'width' : 1550
+            });
+        }
+    }
+
+    bodyWidthSet();
+
+    $(window).resize(function () {
+        bodyWidthSet();
     });
 
-    if($('body').width() < 1550) {
-        $('body').css({
-            'width' : 1550
-        });
-    }
-}
 
-// bodyWidthSet();
+    // header fixed일때 가로스크롤 시 보이게
+    $(window).scroll(function() {
+        $('#header').css({left: 0 - $(this).scrollLeft()});
+    });
 
-$(window).resize(function () {
-    // bodyWidthSet();
-});
 
-// header fixed일때 가로스크롤 시 보이게
-$(window).scroll(function() {
-    $('#header').css({left: 0 - $(this).scrollLeft()});
+    // depth2 hover
+    $('.depth-wrap').on('mouseover', function(){
+        $(this).addClass('on');
+
+        $(this).on('mouseleave', function(){
+            $(this).removeClass('on');
+        })
+    });
+
 });
